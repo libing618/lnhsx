38dbb753112d4efbb46a5014579ed4a6c70ddb4e
libing618 <997098144@qq.com>
2016-11-25 03:02:23 +0800

    11.25

--- pages/brand/brand.js
+++ pages/brand/brand.js
@@ -1,14 +1,16 @@
+const AV = require('../../utils/av-weapp-min.js');
 var app = getApp()
 Page({
     data: {
         indicatorDots: true,
         vertical: false,
         autoplay: true,
-		minusStatuses: ['disabled', 'disabled', 'normal', 'normal', 'disabled'],
+		minusStatuse: 'disabled',
         interval: 3000,
         duration: 1200,
 		fQuantity: 1,
-		cartData:[],
+		brandList:[],
+		cartobjectId: null,
         actionSheetHidden: true
     },
     jCartTap: function(){                        //点“加入购物车”
@@ -17,7 +19,7 @@ Page({
         })
 		var query = new AV.Query('cartData');
 		var priorityQuery = new AV.Query('cartData');
-		priorityQuery.equalTo('fGid', brandList.objectId);        //商品Id
+		priorityQuery.equalTo('fGid', this.data.brandList.objectId);        //商品Id
 		var statusQuery = new AV.Query('cartData');
 		statusQuery.equalTo('fStatus','1');               //状态1购物车、2拼团、3订单、4付款、5发货、6收货、7退货、8清算、9结算
 		var query = AV.Query.and(priorityQuery, statusQuery);
@@ -25,11 +27,12 @@ Page({
 			this.setData({fQuantity : results.fNum})
 		}).then( function(results) {
 			if (results.length == 0){
-				this.setData({fQuantity: 1})
+				this.setData({fQuantity: 1});
 			}
 			else
 			{
-				cartData = results
+				cartobjectId = results.objectId;
+				this.setData({fQuantity: results.get('fNum')});
 			}
 		})
 	},
@@ -38,64 +41,41 @@ Page({
             actionSheetHidden: true
         })
 		//写数据
+		var wCartData = AV.object.extend('cartData');
+		wCartData.set('fGid', brandList.objectId);
+		wCartData.set('fNum', fQuantity);
+		wCartData.set('fPrice', brandList.fPrice);
+		wCartData.set('fShopId', brandList.fShopID);
+		wCartData.set('fUserId', app.globalData.lcUser._id);
+		wCartData.set('fStatus', '1');
+		if (cartobjectId != null){
+			wCartData.set('objectId', cartobjectId);
+		} 
+		wCartData.save().then(function(results){
+			console.log()
+		}),function(error) {
+			console.error(error)
+		}
     },
     bindMinus: function(e) {
-		var index = parseInt(e.currentTarget.dataset.index);
-		var num = this.data.carts[index].get('quantity');
 		// 如果只有1件了，就不允许再减了
-		if (num > 1) {
-			num --;
+		if (fQuantity > 1) {
+			this.setData({fQuantity: fQuantity --});
 		}
 		// 只有大于一件的时候，才能normal状态，否则disable状态
-		var minusStatus = num <= 1 ? 'disabled' : 'normal';
-		// 购物车数据
-		var carts = this.data.carts;
-		carts[index].set('quantity', num);
-		// 按钮可用状态
-		var minusStatuses = this.data.minusStatuses;
-		minusStatuses[index] = minusStatus;
-		// 将数值与状态写回
-		this.setData({
-			carts: carts,
-			minusStatuses: minusStatuses
-		});
-		// update database
-		carts[index].save();
-		this.sum();
+		this.setData({minusStatus: fQuantity <= 1 ? 'disabled' : 'normal'}) ;
 	},
 	bindPlus: function(e) {
-		var index = parseInt(e.currentTarget.dataset.index);
-		var num = this.data.carts[index].get('quantity');
 		// 自增
-		num ++;
+		this.setData({fQuantity: fQuantity ++});
 		// 只有大于一件的时候，才能normal状态，否则disable状态
-		var minusStatus = num <= 1 ? 'disabled' : 'normal';
-		// 购物车数据
-		var carts = this.data.carts;
-		carts[index].set('quantity', num);
-		// 按钮可用状态
-		var minusStatuses = this.data.minusStatuses;
-		minusStatuses[index] = minusStatus;
-		// 将数值与状态写回
-		this.setData({
-			carts: carts,
-			minusStatuses: minusStatuses
-		});
-		// update database
-		carts[index].save();
-		this.sum();
+		this.setData({minusStatus: fQuantity <= 1 ? 'disabled' : 'normal'}) ;
 	},
 	bindManual: function(e) {
-		var index = parseInt(e.currentTarget.dataset.index);
-		var carts = this.data.carts;
-		var num = e.detail.value;
-		carts[index].set('quantity', num);
 		// 将数值与状态写回
 		this.setData({
-			carts: carts
+			fQuantity: e.detail.value
 		});
-		cart[index].save();
-		console.log(this.data.carts);
 	},
         // 选择商品
     onLoad: function(options) {
@@ -111,6 +91,10 @@ Page({
                   }
              }
         }
+		else
+		{
+			wx.navigateBack();
+		}
     }
 
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,22 +1,59 @@
-var lc = require( '../../utils/lcgetdata.js');
+const AV = require('../../utils/av-weapp-min.js');
+function upData(classdata,newdata) {
+    if (newdata.length>0) {                                                //有加载新增则更新数据
+        for (var i=0; i<newdata.length; i++){
+            for (var x=0; x<classdata.length; x++){                       //查找重复ID并删除旧记录
+                if (newdata[i].objectId==classdata[x].objectId){          //每个对象的记录中都有objectId
+                    classdata.splice(x, 1);
+                }
+            }    
+            classdata.unshift(newdata[i]);
+        }
+        return classdata
+    };
+}
+
+function dowData(classdata,newdata) {
+    if (newdata.length>0) {                                                //有新增刷新则更新数据
+        for (var i=newdata.length; i=0; i--){
+            for (var x=0; x<classdata.length; x++){                       //查找重复ID并删除旧记录
+                if (newdata[i-1].objectId==classdata[x].objectId){          //每个对象的记录中都有objectId
+                    classdata.splice(x, 1);
+                }
+            }    
+            classdata.push(newdata[i]);
+        }
+        return classdata
+    };
+}
 
 //获取应用实例
 var app = getApp()
 Page({
     data: {
+        img: [],
+        goods: [],
         indicatorDots: false,
         vertical: false,
         interval: 3000,
         duration: 1200,
+        scrollTop : 0,
+        scrollHeight:0,
         loadingHidden: false  // loading
     },
     onLoad: function() {
     //事件处理函数
         var that = this            //调用应用实例的方法获取全局数据
-        lc.lcgetList(that,'img');
-                autoplay: true
-
-        lc.lcgetList(that,'goods');
+        wx.getSystemInfo({
+            success:function(res){
+                    //这里要非常注意，微信的scroll-view必须要设置高度才能监听滚动事件，所以，需要在页面的onLoad事件中给scroll-view的高度赋值
+                that.setData({
+                    scrollHeight: res.windowHeight
+                });
+            }
+        });
+        that.setData({ img: wx.getStorageSync('imgdata') || [] });
+        that.setData({ goods: wx.getStorageSync('goodsdata') || [] });
 
         //choiceList
         wx.request({
@@ -28,7 +65,7 @@ Page({
             },
             success: function(res) {
                 that.setData({
-                    choiceItems: res.data.data.dataList
+                    choiceItems: res.data.data.dataList.slice(0,5)
                 })
                 setTimeout(function () {
                     that.setData({
@@ -40,15 +77,119 @@ Page({
     },
 
     onShow: function () {
+        var that = this;
+        var gxdate = '1970-01-01' 
+        if (img != []){
+             gxdate = img[0].updatedAt ;
+        };
+        var pimgData = {
+            'classname': 'imgData',
+            'skipd': '6',
+            'gxdate': gxdate,
+            'ksdate': Date.now
+        };
+        AV.Cloud.run('lhndata',pimgData).then(function(results) {
+            that.setData({
+                img: upData(img,results),
+                autoplay: true          //从index.wxml的class="swiper_box"中删掉bindchange="swiperchange"否则总报警
+            })
+        },function(error){
+            console.error(error)
+        });
+        var pgoodsData = {
+            'classname': 'goodsData',
+            'skipd': '10',
+            'gxdate': (goods==null) ? '1970-01-01' : goods[0].updatedAt,
+            'ksdate': Date.now
+        };
+        AV.Cloud.run('lhndata',pgoodsData).then(function(results) {
+            that.setData({
+                goods: upData(goods,results)
+            })
+        },function(error){
+            console.error(error)
+        });
+    },
+
+    bindDownLoad:function(){
+        //   该方法绑定了页面滑动到底部的事件
+        var that = this;
+        that.setData({
+            loadingHidden: false,
+        });
+        var pgoodsData = {
+            'classname': 'goodsData',
+            'skipd': '10',
+            'gxdate': '1970-01-01' ,
+            'ksdate': goods[goods.length-1].updatedAt
+        };
+        AV.Cloud.run('lhndata',pgoodsData).then(function(results) {
+            that.setData({
+                goods: downData(goods,results),
+            })
+            setTimeout(function () {
+                that.setData({
+                    loadingHidden: true
+                })
+            }, 1500)
+        },function(error){
+            console.error(error)
+        });
+    },
+
+    scroll:function(event){
+        //   该方法绑定了页面滚动时的事件，这里记录当前的position.y的值,为了请求数据之后把页面定位到这里来。
         this.setData({
-            autoplay: true          //从index.wxml的class="swiper_box"中删掉bindchange="swiperchange"否则总报警
-        })
+            scrollTop : event.detail.scrollTop
+        });
+    },
+
+    refresh:function(event){
+        //   该方法绑定了页面滑动到顶部的事件，然后做上拉刷新
+        var that = this;
+        that.setData({
+            loadingHidden: false,
+            scrollTop : 0
+        });
+        var pimgData = {
+            'classname': 'imgData',
+            'skipd': '6',
+            'gxdate': img==[] ? '1970-01-01' : img[0].updatedAt,
+            'ksdate': Date.now
+        };
+        AV.Cloud.run('lhndata',pimgData).then(function(results) {
+            this.setData({
+                img: upData(img,results),
+            })
+        },function(error){
+            console.error(error)
+        });
+        var pgoodsData = {
+            'classname': 'goodsData',
+            'skipd': '10',
+            'gxdate': goods==[] ? '1970-01-01' : goods[0].updatedAt,
+            'ksdate': Date.now
+        };
+        AV.Cloud.run('lhndata',pgoodsData).then(function(results) {
+            that.setData({
+                goods: upData(goods,results)
+            })
+            setTimeout(function () {
+                that.setData({
+                    loadingHidden: true
+                })
+            }, 1500)
+        },function(error){
+            console.error(error)
+        });
     },
 
     onHide: function () {
         this.setData({
-            autoplay: false              //退出当前页时关闭自动轮播
+            autoplay: false              //退出当前页时关闭自动轮播,缓存数据。
         });
+        wx.setStorage('imgdata', img);
+        wx.setStorage('goodsdata', goods);
     },
 
 })
